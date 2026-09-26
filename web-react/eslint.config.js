b50077e42aa450c6862845c01da2738232c6b89b
Biswajit Jana <bj7585063100@gmail.com>
2026-09-26 07:20:24 +0100

    build: harden reproducible verification pipeline

--- web-react/eslint.config.js
+++ web-react/eslint.config.js
@@ -1,6 +1,5 @@
 import js from '@eslint/js';
 import globals from 'globals';
-import react from 'eslint-plugin-react';
 import hooks from 'eslint-plugin-react-hooks';
 
 export default [
@@ -9,13 +8,9 @@ export default [
   {
     files: ['**/*.{js,jsx}'],
     languageOptions: { ecmaVersion: 2022, sourceType: 'module', globals: globals.browser, parserOptions: { ecmaFeatures: { jsx: true } } },
-    plugins: { react, 'react-hooks': hooks },
-    settings: { react: { version: 'detect' } },
+    plugins: { 'react-hooks': hooks },
     rules: {
       ...hooks.configs.recommended.rules,
-      'react/react-in-jsx-scope': 'off',
-      'react/jsx-uses-vars': 'error',
-      'react/jsx-uses-react': 'error',
       'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
     },
   },
